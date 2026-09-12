824a1532f3445b45516448f21c17c643bc80e556
yuttapols <myprofile.yuttapol@gmail.com>
2026-09-13 02:45:43 +0700

    Frist Commit

---
+++ src/main/resources/db/migration/V3__fix_menu_icons.sql
@@ -0,0 +1,7 @@
+UPDATE menu_items SET icon = 'pi-home' WHERE menu_key = 'menu.dashboard' AND icon = 'dashboard';
+UPDATE menu_items SET icon = 'pi-user' WHERE menu_key = 'menu.profile' AND icon = 'person';
+UPDATE menu_items SET icon = 'pi-cog' WHERE menu_key = 'menu.admin' AND icon = 'admin_panel_settings';
+UPDATE menu_items SET icon = 'pi-shop' WHERE menu_key = 'menu.supervisor' AND icon = 'store';
+UPDATE menu_items SET icon = 'pi-users' WHERE menu_key = 'menu.admin.supervisors' AND icon = 'supervisor_account';
+UPDATE menu_items SET icon = 'pi-history' WHERE menu_key = 'menu.admin.loginLogs' AND icon = 'history';
+UPDATE menu_items SET icon = 'pi-users' WHERE menu_key = 'menu.supervisor.employees' AND icon = 'groups';
